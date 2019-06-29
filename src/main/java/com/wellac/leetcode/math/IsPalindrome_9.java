e3139dd6df95c2c310e6fa881da1ff884dbc6ea2
wellac <wellac01@163.com>
2019-06-29 22:46:23 +0800

    9. 回文数

---
+++ src/main/java/com/wellac/leetcode/math/IsPalindrome_9.java
@@ -0,0 +1,22 @@
+package com.wellac.leetcode.math;
+
+/**
+ * 9. 回文数
+ */
+public class IsPalindrome_9 {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reverse = 0;
+
+        while (reverse < x) {
+            reverse = reverse * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return x == reverse || x == (reverse / 10);
+    }
+}
