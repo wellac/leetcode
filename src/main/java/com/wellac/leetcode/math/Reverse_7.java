f29ec288bfb0ea88b1f8a3c923be16dc0464cb07
wellac <wellac01@163.com>
2019-07-14 18:33:31 +0800

    ...

--- src/main/java/com/wellac/leetcode/math/Reverse_7.java
+++ src/main/java/com/wellac/leetcode/math/Reverse_7.java
@@ -14,7 +14,6 @@ public class Reverse_7 {
 
     public int reverse(int x) {
         int res = 0;
-        boolean f = x > 0;
         while (x != 0) {
             if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && x % 10 > 7)) return 0;
             if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && x % 10 < -8)) return 0;
