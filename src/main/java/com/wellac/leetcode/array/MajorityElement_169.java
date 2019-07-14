1b7e2eef85b06cf50783ae9ba618c6a753d9349b
wellac <wellac01@163.com>
2019-07-14 23:16:38 +0800

    169. 求众数(优化解法)

--- src/main/java/com/wellac/leetcode/array/MajorityElement_169.java
+++ src/main/java/com/wellac/leetcode/array/MajorityElement_169.java
@@ -6,8 +6,23 @@ import java.util.Arrays;
  * 169. 求众数
  */
 public class MajorityElement_169 {
+
+    //Boyer-Moore 投票算法
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        int major = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (count==0){
+                major = nums[i];
+                count++;
+                continue;
+            }
+            if (major == nums[i]) {
+                count++;
+            }else {
+                count--;
+            }
+        }
+        return major;
     }
 }
