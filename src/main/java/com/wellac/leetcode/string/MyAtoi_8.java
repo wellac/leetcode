95bf982c445fbc4d67779891d245e66ab339f5d5
wellac <wellac01@163.com>
2019-09-07 14:21:44 +0800

    8. 字符串转换整数 (atoi)（优化解）

--- src/main/java/com/wellac/leetcode/string/MyAtoi_8.java
+++ src/main/java/com/wellac/leetcode/string/MyAtoi_8.java
@@ -10,78 +10,45 @@ import java.util.HashSet;
  * 8. 字符串转换整数 (atoi)
  */
 public class MyAtoi_8 {
-    HashSet<Character> numberSet = new HashSet<>(Arrays.asList('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
-    HashSet<Character> set = new HashSet<>(Arrays.asList('+', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
-    HashMap<Character, Integer> map = new HashMap<>();
-
-    {
-        map.put('0', 0);
-        map.put('1', 1);
-        map.put('2', 2);
-        map.put('3', 3);
-        map.put('4', 4);
-        map.put('5', 5);
-        map.put('6', 6);
-        map.put('7', 7);
-        map.put('8', 8);
-        map.put('9', 9);
-    }
-
     @Test
     public void test() {
-        System.out.println(myAtoi(" "));
+        System.out.println(myAtoi("-2147483649"));
     }
 
     public int myAtoi(String str) {
-        System.out.println(str);
+        //处理特殊情况
+        if (str == null) return 0;
+        str = str.trim();
+        if (str.equals("")) return 0;
+
+        //获取符号位
+        int flag = 1;
+        int curr = 0;
+        char firstChar = str.charAt(curr);
+        if (firstChar == '-') {
+            flag = -1;
+            curr++;
+        } else if (firstChar == '+') {
+            curr++;
+        } else if (!Character.isDigit(firstChar)) {
+            return 0;
+        }
+
+        //开始转换
         int res = 0;
-
-        if (str.length() == 0) return res;
-
-        int start = 0;
-        for (; start < str.length(); start++) {
-            if (str.charAt(start) != ' ') break;
-        }
-
-        if (str.length() - 1 < start) return res;
-
-        str = str.substring(start);
-
-        System.out.println(str);
-
-
-        if (!numberSet.contains(str.charAt(0))) {
-            if (str.charAt(0) == '+') {
-                res = 1;
-            } else if (str.charAt(0) == '-') {
-                res = -1;
-            } else {
-                return 0;
+        while (curr < str.length()) {
+            if (!Character.isDigit(str.charAt(curr))) {
+                return res;
             }
-            str = str.substring(1);
-        } else {
-            res = 1;
-        }
-
-        int num = 0;
-        for (int i = 0; i < str.length(); i++) {
-            if (!numberSet.contains(str.charAt(i))) break;
-            if (Integer.MAX_VALUE / 10 < num || (Integer.MAX_VALUE / 10 == num && map.get(str.charAt(i)) > 7)) {
-                if (res == 1) {
-                    return Integer.MAX_VALUE;
-                } else {
-                    if (Integer.MAX_VALUE / 10 < num)
-                        return Integer.MIN_VALUE;
-                    else if (map.get(str.charAt(i)) > 8)
-                        return Integer.MIN_VALUE;
-                }
-
+            int num = flag * (str.charAt(curr++) - '0');
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && num > Integer.MAX_VALUE % 10)) {
+                return Integer.MAX_VALUE;
             }
-
-            num = num * 10 + map.get(str.charAt(i));
+            if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && num < Integer.MIN_VALUE % 10)) {
+                return Integer.MIN_VALUE;
+            }
+            res = res * 10 + num;
         }
-
-
-        return res * num;
+        return res;
     }
 }
