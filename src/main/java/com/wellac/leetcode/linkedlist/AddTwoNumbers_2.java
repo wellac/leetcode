94e564e9c172d41b993f80b7889803f84cb55954
wellac <wellac01@163.com>
2019-09-07 08:32:51 +0800

    2.两数相加(优化解法)

--- src/main/java/com/wellac/leetcode/linkedlist/AddTwoNumbers_2.java
+++ src/main/java/com/wellac/leetcode/linkedlist/AddTwoNumbers_2.java
@@ -1,36 +1,26 @@
 package com.wellac.leetcode.linkedlist;
 
 /**
- * 两数相加
+ * 2.两数相加
  */
 public class AddTwoNumbers_2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(0);
         ListNode tail = dummy;
-        int temp = 0;
+        int carry = 0;//记录进位
+        while (l1 != null || l2 != null || carry != 0) {
+            //带进位求和，并计算进位
+            int sum = carry + (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
+            carry = sum / 10;
 
-        while (l1 != null || l2 != null) {
-            int v1 = l1 != null ? l1.val : 0;
-            int v2 = l2 != null ? l2.val : 0;
-
-            int num = (v1 + v2 + temp) % 10;
-            temp = (v1 + v2 + temp) / 10;
-
-            ListNode tempNode = new ListNode(num);
-            tail.next = tempNode;
+            //尾插法插入新节点
+            tail.next = new ListNode(sum % 10);
             tail = tail.next;
 
-            l1 = l1 != null ? l1.next : null;
-            l2 = l2 != null ? l2.next : null;
+            //向后移动l1,l2
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
         }
-
-        if (temp != 0) {
-            ListNode tempNode = new ListNode(temp);
-            tail.next = tempNode;
-            tail = tail.next;
-        }
-
         return dummy.next;
-
     }
 }
