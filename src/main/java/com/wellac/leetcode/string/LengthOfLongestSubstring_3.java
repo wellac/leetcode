6f51ed01cdc1ff5352e40ed3e053977d13be1657
wellac <wellac01@163.com>
2019-09-07 08:48:11 +0800

    3. 无重复字符的最长子串（优化解）

--- src/main/java/com/wellac/leetcode/string/LengthOfLongestSubstring_3.java
+++ src/main/java/com/wellac/leetcode/string/LengthOfLongestSubstring_3.java
@@ -4,47 +4,28 @@ import org.junit.Test;
 
 import java.util.*;
 
+/**
+ * 3. 无重复字符的最长子串
+ */
 public class LengthOfLongestSubstring_3 {
 
     @Test
     public void test() {
         String s = "abcabcbb";
-        System.out.println(lengthOfLongestSubstring_1(s));
+        System.out.println(lengthOfLongestSubstring(s));
     }
 
-    public int lengthOfLongestSubstring_1(String s) {
-        int n = s.length(), ans = 0;
+    public int lengthOfLongestSubstring(String s) {
+        int res = 0;
         Map<Character, Integer> map = new HashMap<>();
-        for (int end = 0, start = 0; end < n; end++) {
-            char alpha = s.charAt(end);
-            if (map.containsKey(alpha)) {
-                start = Math.max(map.get(alpha), start);
+        //(i,j)维护一个无重复元素的字串
+        for (int i = 0, j = 0; j < s.length(); j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(i, map.get(s.charAt(j)) + 1);
             }
-            ans = Math.max(ans, end - start + 1);
-            map.put(s.charAt(end), end + 1);
+            map.put(s.charAt(j), j);
+            res = Math.max(res, j - i + 1);
         }
-        return ans;
-    }
-
-    public int lengthOfLongestSubstring_2(String s) {
-
-        if (s.equals("")) return 0;
-
-        List<Character> charList = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            charList.add(s.charAt(i));
-        }
-        int windowSize = new HashSet<>(charList).size();
-
-
-        while (windowSize != 1) {
-            for (int i = 0; i < s.length() - windowSize + 1; i++) {
-                if (new HashSet<>(charList.subList(i, i + windowSize)).size() == windowSize) {
-                    return windowSize;
-                }
-            }
-            windowSize--;
-        }
-        return windowSize;
+        return res;
     }
 }
