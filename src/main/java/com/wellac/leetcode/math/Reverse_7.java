71526a80063f36f782d2616e4f1fd08088b638a7
wellac <wellac01@163.com>
2019-09-07 13:36:47 +0800

    7. 整数反转（优化解）

--- src/main/java/com/wellac/leetcode/math/Reverse_7.java
+++ src/main/java/com/wellac/leetcode/math/Reverse_7.java
@@ -15,9 +15,12 @@ public class Reverse_7 {
     public int reverse(int x) {
         int res = 0;
         while (x != 0) {
-            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && x % 10 > 7)) return 0;
-            if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && x % 10 < -8)) return 0;
-
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && x % 10 > Integer.MAX_VALUE % 10)){
+                return 0;
+            }
+            if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && x % 10 < Integer.MIN_VALUE % 10)){
+                return 0;
+            }
             res = res * 10 + x % 10;
             x = x / 10;
         }
