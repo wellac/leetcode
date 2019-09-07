336b442cfb353b99cb319b86aa08c3b34b8bee3a
wellac <wellac01@163.com>
2019-09-07 09:58:47 +0800

    5. 最长回文子串（优化解法）

--- src/main/java/com/wellac/leetcode/dp/LongestPalindrome_5.java
+++ src/main/java/com/wellac/leetcode/dp/LongestPalindrome_5.java
@@ -9,59 +9,27 @@ public class LongestPalindrome_5 {
 
     @Test
     public void test() {
-        System.out.println(longestPalindrome_2("babc"));
+        System.out.println(longestPalindrome("babc"));
     }
 
-    //暴力解法，时间复杂度o（n^2)
-    public String longestPalindrome_1(String s) {
-        if (s.equals("")) return "";
-        int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j < s.length(); j++) {
-                if (isPalindrome(s, i, j) && end - start < j - i) {
-                    start = i;
-                    end = j;
-                }
-            }
-        }
-        return s.substring(start, end + 1);
-    }
-
-    //动态规划解法
-    public String longestPalindrome_2(String s) {
-        if (s.equals("")) return "";
-
+    public String longestPalindrome(String s) {
         int len = s.length();
+        int max_len = 0;
+        int start = 0;
         boolean[][] dp = new boolean[len][len];
 
-        int start = 0;//最长回文子串的起始下标
-        int max = 1;//最长回文子串的长度
-        for (int i = 0; i < len; i++) {
-            dp[i][i] = true;
-            if (i < len - 1 && s.charAt(i) == s.charAt(i + 1)) {
-                dp[i][i + 1] = true;
-                max = 2;
-                start = i;
-            }
-        }
-        for (int w = 3; w <= len; w++) {
-            for (int i = 0; i <= len - w; i++) {
-                if (s.charAt(i) == s.charAt(i + w - 1) && dp[i + 1][i + w - 2]) {
-                    dp[i][i + w - 1] = true;
-                    max = w;
+        for (int w = 1; w <= len; w++) {
+            for (int i = 0, j = i + w - 1; j < len; i++, j++) {
+                dp[i][j] = (w == 1)
+                        || (w == 2 && s.charAt(i) == s.charAt(j))
+                        || (dp[i + 1][j - 1] && s.charAt(i) == s.charAt(j));
+
+                if (dp[i][j]) {
+                    max_len = w;
                     start = i;
                 }
             }
         }
-        return s.substring(start, start + max);
-    }
-
-    private boolean isPalindrome(String s, int start, int end) {
-        while (start < end) {
-            if (s.charAt(start++) != s.charAt(end--)) {
-                return false;
-            }
-        }
-        return true;
+        return s.substring(start, start + max_len);
     }
 }
