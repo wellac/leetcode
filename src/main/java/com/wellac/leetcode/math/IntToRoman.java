57f872f08308818bc50879622d22a3bf364e7871
wellac <wellac01@163.com>
2019-09-08 11:15:40 +0800

    12. 整数转罗马数字（优化解）

--- src/main/java/com/wellac/leetcode/math/IntToRoman.java
+++ src/main/java/com/wellac/leetcode/math/IntToRoman.java
@@ -15,16 +15,13 @@ public class IntToRoman {
 
     public String intToRoman(int num) {
         int[] value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] ch = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String[] chs = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
         StringBuilder res = new StringBuilder();
-        for (int i = 0; i < value.length && num != 0; i++) {
-            int temp = num / value[i];
-            if (temp > 0) {
-                for (int j = temp; j > 0; j--) {
-                    res.append(ch[i]);
-                }
-                num -= value[i] * temp;
+        for (int i = 0; i < value.length; i++) {
+            while (num >= value[i]) {
+                res.append(chs[i]);
+                num = num - value[i];
             }
         }
         return res.toString();
