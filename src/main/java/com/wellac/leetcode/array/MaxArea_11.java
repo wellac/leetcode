37269adda4640e790c06ecb4dabdc4e886fdb1fd
wellac <wellac01@163.com>
2019-09-08 09:29:52 +0800

    11. 盛最多水的容器（优化解）

--- src/main/java/com/wellac/leetcode/array/MaxArea_11.java
+++ src/main/java/com/wellac/leetcode/array/MaxArea_11.java
@@ -9,9 +9,18 @@ public class MaxArea_11 {
 
         while (start < end) {
             max = Math.max(max, (end - start) * Math.min(height[start], height[end]));
+            /**
+             * 情形1：一高一矮
+             *      移动较矮的一侧（相较于暴力法，过滤掉移动较高侧无法带来增益的情形）
+             * 情形2：两侧同高
+             *      同时移动两侧（移动任何一侧都无法带来增益）
+             */
             if (height[start] < height[end]) {
                 start++;
+            } else if (height[start] > height[end]) {
+                end--;
             } else {
+                start++;
                 end--;
             }
         }
