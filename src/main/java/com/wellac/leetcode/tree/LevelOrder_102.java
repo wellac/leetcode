e832c3faf224b33a1bc0d87eaa1e04c7f2ad89ae
wellac <wellac01@163.com>
2019-10-09 16:42:31 +0800

    144.二叉树的层次遍历（优化解）

--- src/main/java/com/wellac/leetcode/tree/LevelOrder_102.java
+++ src/main/java/com/wellac/leetcode/tree/LevelOrder_102.java
@@ -3,41 +3,33 @@ package com.wellac.leetcode.tree;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
- * 二叉树的层次遍历
+ * 144.二叉树的层次遍历
  */
 public class LevelOrder_102 {
 
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        if(root==null) return result;
+        if (root == null) return result;
 
-        LinkedList<TreeNode> s = new LinkedList<>();
-        LinkedList<TreeNode> d = new LinkedList<>();
-        s.add(root);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);//根节点入队
 
-        while(!s.isEmpty() || !d.isEmpty()){
-            List<Integer> temp = new ArrayList();
-            if(!s.isEmpty()){
-                while(!s.isEmpty()){
-                    TreeNode t = s.poll();
-                    temp.add(t.val);
-                    if(t.left != null) d.add(t.left);
-                    if(t.right != null) d.add(t.right);
-                }
-            }else{
-                while(!d.isEmpty()){
-                    TreeNode t = d.poll();
-                    temp.add(t.val);
-                    if(t.left != null) s.add(t.left);
-                    if(t.right != null) s.add(t.right);
-                }
+        while (!queue.isEmpty()) {
+            int size = queue.size();//记录当前层节点数量
+            ArrayList<Integer> subList = new ArrayList<>();//记录当前层节点数据
+            for (int i = 0; i < size; i++) {
+                TreeNode t = queue.poll();//出队队首元素
+                subList.add(t.val);
+                if (t.left != null) queue.add(t.left);//先左孩子入队
+                if (t.right != null) queue.add(t.right);//再右孩子入队
             }
-            result.add(temp);
+            result.add(subList);
         }
 
         return result;
-
     }
+
 }
